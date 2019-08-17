0425f128c4ac3b126772433f8e96189b04c13720
Javier Mayol <carlosjmayol@gmail.com>
2019-08-17 18:24:54 +0000

    added forms files and templates

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,12 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, flash, redirect, url_for
+from config import Config
+from forms import LoginForm
 
+app = Flask(__name__)
+app.config.from_object(Config)
 @app.route('/')
 @app.route('/index')
 def index():
@@ -20,3 +23,12 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
