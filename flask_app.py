f8d8b65fc9ecfda97ceecb32e2739a7eb6e1a0b2
Javier Mayol <carlosjmayol@gmail.com>
2019-08-20 20:22:08 +0000

    Added user login and register

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,17 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from config import Config
-from forms import LoginForm
+from werkzeug.urls import url_parse
 
 app = Flask(__name__)
 app.config.from_object(Config)
 login = LoginManager(app)
+login.login_view = 'login'
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="endlessLoop",
@@ -26,12 +27,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from models import User, Post
-
+from forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Javier'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -43,16 +44,53 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.shell_context_processor
 def make_shell_context():
